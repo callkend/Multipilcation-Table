76068e68fd476f196e6edecc39d282821dde7747
callkend <77285430+callkend@users.noreply.github.com>
2021-01-26 07:55:04 -0700

    Add project files.

---
+++ Multipilcation Table.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim tableSize As Int16
+        Dim userInput As String
+        Dim problem As Boolean = True
+
+        'prompts user for the size of the Multiplication table and checks for valid input
+        Do Until problem = False
+            Try
+                Console.WriteLine("Enter the size of Table")
+                userInput = Console.ReadLine()
+                problem = False
+                tableSize = CDec(userInput)
+            Catch ex As Exception
+                Console.WriteLine($"{userInput} is invaild. Enter a Number")
+                problem = True
+            End Try
+        Loop
+
+        Console.WriteLine($"Here is a {tableSize} * {tableSize} Multiplication Table")
+
+        'Creates the Multipication Table
+        For j = 1 To tableSize
+            For i = 1 To tableSize
+                Console.Write(j * i)
+                Console.Write(vbTab)
+            Next
+            Console.WriteLine()
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module
